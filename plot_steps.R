9c5e706229922e2529293c0da8ab73dfc0b6c7a4
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2013-10-02 20:45:31 -0400

    update

--- plot_steps.R
+++ plot_steps.R
@@ -1,6 +1,7 @@
 dm <- read.csv("/Volumes/Echo/Dropbox/GIS/Data/Monitoring/JawboneUp/2013.csv",header=T) 
 #dm <- read.table(text = Lines, header = TRUE)
-dm$DATE <- as.Date(dm$DATE, "%Y %m %d")
+work_date <- dm$DATE
+dm$DATE <- as.Date(as.character(work_date), format("%Y%m%d"))
 plot(m_steps ~ DATE, dm, xaxt = "n", type = "l")
 axis(1, dm$DATE, , cex.axis = .7)
 
